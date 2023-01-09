e40e08bb05b1cebf3a039bc974bde21d094a6e8e
Дмитрий Канский <ernokanst@PowerBook-G6.local>
2023-01-09 21:55:18 +0300

    Home page prototype

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
     <v-app-bar :elevation="6" color="primary">
-        <v-app-bar-title><h1>Онлайн-школа</h1></v-app-bar-title>
+        <v-app-bar-title><h2>Онлайн-школа</h2></v-app-bar-title>
+        <template v-slot:append>
+          <v-btn prepend-icon="mdi-account-tie" >Аккаунт</v-btn>
+        </template>
     </v-app-bar>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,52 @@
 <template>
-  <HelloWorld />
+  <div class="infoblock">
+    <h1>Добро пожаловать!</h1>
+    <p>Выберите курс и приступайте к обучению!</p>
+  </div>
+  <v-card
+    class="mx-auto"
+  >
+
+    <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="align-end"
+              height="400px"
+              cover
+            >
+            </v-img>
+            
+            <v-card-actions>
+              
+              <v-card-title class="text" v-text="card.title"></v-card-title>
+
+              <v-spacer></v-spacer>
+
+              <v-btn size="small" color="surface-variant" variant="text" icon="mdi-bookmark"></v-btn>
+
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
-<script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+<script>
+  export default {
+    data: () => ({
+      cards: [
+        { title: 'Физика', src: 'https://get.pxhere.com/photo/light-museum-show-human-blue-electricity-lightning-energy-high-voltage-thunder-flash-thunderstorm-demonstration-experiment-physics-discharge-artificial-lightning-museum-of-technology-tesla-coil-electrical-supply-experimental-physics-electric-shielding-nikola-tesla-tesla-transformer-resonance-transformer-high-frequency-ac-voltage-faradayscher-cage-faraday-cage-faraday-speed-1126030.jpg', flex: 4 },
+        { title: 'Информатика', src: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Computer-science-education.jpg', flex: 4 },
+        { title: 'Английский язык', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 4 },
+      ],
+    }),
+  }
 </script>
